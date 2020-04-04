7aa78753bf1a698f34367081522d99e99d7723bb
Sylvain Bouchard <sylvain.bouchard@econocart.ca>
2020-04-04 09:59:35 -0400

    fix: various adjustments

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { AqiService } from './aqi/aqi.service';
 import { EventService } from './event/event.service';
 import { ConfigService } from './config/config.service';
 import { EnvironmentService } from './environment/environment.service';
+import { AqiPublisher } from './aqi/aqiPublisher';
+import { NodesPublisher } from './environment/nodesPublisher';
 
 @Module({
   imports: [],
   controllers: [AppController, AqiController],
-  providers: [AppService, AqiService, EventService, ConfigService, EnvironmentService],
+  providers: [AppService, AqiPublisher, AqiService, EventService, ConfigService, EnvironmentService, NodesPublisher],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AqiPublisher } from './aqi/aqiPublisher';
 import { AppModule } from './app.module';
+import { NodesPublisher } from './environment/nodesPublisher';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('AQI Sensor Controller')
+    .setDescription(
+      'Welcome to air quality sensor remote controller API',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  const aqiPublisher = app.get(AqiPublisher);
+  aqiPublisher.start();
+
+  const nodesPublisher = app.get(NodesPublisher);
+  nodesPublisher.start();
+
+  await app.listen(3500);
 }
 bootstrap();
